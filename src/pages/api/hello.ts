dfef1628a60e707d968d72c62dc575e9d579c4bd
Slottey <rittikiert.s@gmail.com>
2024-12-18 21:38:21 +0700

    Update

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    res.status(200).json("Success");
+  } catch (error: any) {
+    console.log("Error ==>", error?.message);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 "use client";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { api } from "@/utils/api";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
+
+  const test = async () => {
+    try {
+      const res = await api.get("/api/hello");
+
+      console.log("res", res.data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   useEffect(() => {
-    router.push("/login");
+    test();
   }, []);
+
+  // useEffect(() => {
+  //   router.push("/login");
+  // }, []);
 
   return <div className="flex"></div>;
 }
