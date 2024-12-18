521418ef5aaae380cb3c01d19d96475a3c85f3de
Slottey <rittikiert.s@gmail.com>
2024-12-18 22:59:06 +0700

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,13 @@
 "use client";
 import React, { useEffect } from "react";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  // const router = useRouter();
-
-  const test = async () => {
-    try {
-      const res = await fetch("/api/hello", {
-        method: "POST",
-      });
-      const response = await res.json();
-      console.log("response==>", response);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+  const router = useRouter();
 
   useEffect(() => {
-    test();
+    router.push("/login");
   }, []);
-
-  // useEffect(() => {
-  //   router.push("/login");
-  // }, []);
 
   return <div className="flex"></div>;
 }
