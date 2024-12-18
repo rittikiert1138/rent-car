369fbd5d1b7fa4ac635fdef40bc7b8af61c1c59c
Slottey <rittikiert.s@gmail.com>
2024-12-18 22:39:00 +0700

    Update version

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -107,7 +107,7 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-      <div className="fixed right-2 bottom-2">1.0.1</div>
+      <div className="fixed right-2 bottom-2">1.0.2</div>
     </MemberLayout>
   );
 };
