9f95ba49a974f01b696c5b1bb025e199ac77cb37
Slottey <rittikiert.s@gmail.com>
2024-12-18 22:49:47 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
 // import { useRouter } from "next/navigation";
-import { api } from "@/utils/api";
 
 export default function Home() {
   // const router = useRouter();
