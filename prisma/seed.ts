0026a3812fb6d8882394e52dce9e9b28cfa064a5
Slottey <rittikiert.s@gmail.com>
2025-07-01 08:47:57 +0700

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 async function main() {
   const hashPassword = await bcrypt.hash("secret", 10);
-
   await prisma.user.create({
     data: {
-      phone: "0838427291",
-      username: "master",
-      password: hashPassword,
-      role: "MASTER",
-      status: 1,
-      createdBy: 1,
-      updatedBy: 1,
-    },
-  });
-
-  await prisma.user.create({
-    data: {
-      phone: "0838427291",
+      phone: "0838427290",
       username: "master",
       password: hashPassword,
       role: "MASTER",
@@ -30,10 +17,10 @@ async function main() {
 }
 main()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
